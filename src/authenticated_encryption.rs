6554deed77a791879634159c2a0a266d62966585
akritrime <akritrime@gmail.com>
2018-06-18 21:54:24 +0530

    changed comparison for byte arrays with a constant time implementation

--- src/authenticated_encryption.rs
+++ src/authenticated_encryption.rs
@@ -1,8 +1,8 @@
-use hkdf::Hkdf;
-use sha2::Sha256;
-use openssl::{symm, error::ErrorStack};
-use hmac::{Hmac, Mac};
 use clear_on_drop::clear::Clear;
+use hkdf::Hkdf;
+use hmac::{Hmac, Mac};
+use openssl::{error::ErrorStack, symm};
+use sha2::Sha256;
 
 // https://git.matrix.org/git/olm/about/docs/olm.rst
 // Section: The Olm Algortihm, Olm Authenticated Encryption
@@ -14,7 +14,7 @@ type HmacSha256 = Hmac<Sha256>;
 pub struct EncryptionKeys {
     aes_key: Vec<u8>,
     hmac_key: Vec<u8>,
-    aes_iv: Vec<u8>
+    aes_iv: Vec<u8>,
 }
 
 impl Drop for EncryptionKeys {
@@ -26,7 +26,6 @@ impl Drop for EncryptionKeys {
 }
 
 impl EncryptionKeys {
-
     // AES_KEY(i, j) || HMAC_KEY(i, j) || AES_IV(i, j) = HKDF(0, M9i, j), "OLM_KEYS", 80)
     pub fn compute_keys(msg_key: &[u8]) -> Self {
         let salt = [0u8; 64];
@@ -34,16 +33,16 @@ impl EncryptionKeys {
         let length = 80;
         // ikm will be the shared the message key M(i, j)
         let hkdf_extract = Hkdf::<Sha256>::extract(&salt, &msg_key);
-        
+
         //128 bit AES_IV
         //256 bit AES_KEY
         //256 bit HMAC_KEY
         let mut aes_key = hkdf_extract.expand(info.as_bytes(), length);
         assert_eq!(aes_key.len(), 80);
-        
+
         let mut hmac_key = aes_key.split_off(32);
         let aes_iv = hmac_key.split_off(32);
-        
+
         assert_eq!(aes_key.len(), 32);
         assert_eq!(hmac_key.len(), 32);
         assert_eq!(aes_iv.len(), 16);
@@ -51,25 +50,26 @@ impl EncryptionKeys {
         Self {
             aes_key,
             hmac_key,
-            aes_iv
+            aes_iv,
         }
     }
 
     // encrypts plain-text with AES-256, using the key AES_KEY(i, j) and the IV AES_IV(i, j) to give the cipher-text, X(i, j)
-    pub fn encrypt(&self, msg: &[u8]) -> Result<Vec<u8>,ErrorStack> {
+    pub fn encrypt(&self, msg: &[u8]) -> Result<Vec<u8>, ErrorStack> {
         let cipher = symm::Cipher::aes_256_cbc();
         symm::encrypt(cipher, &self.aes_key, Some(&self.aes_iv), msg)
     }
 
     // decrypts cipher_text X(i, j)
-    pub fn decrypt(&self, cipher_text: &[u8]) -> Result<Vec<u8>,ErrorStack> {
+    pub fn decrypt(&self, cipher_text: &[u8]) -> Result<Vec<u8>, ErrorStack> {
         let cipher = symm::Cipher::aes_256_cbc();
         symm::decrypt(cipher, &self.aes_key, Some(&self.aes_iv), cipher_text)
     }
 
     // produces a 8 byte MAC from the input with the HMAC_KEY(i, j). The input shpuld be the entire message(including the Version Bytes and all Payload Bytes)
     pub fn authenticate(&self, input: &[u8]) -> Vec<u8> {
-        let mut mac = HmacSha256::new_varkey(&self.hmac_key).expect("error while creating MAC instance from key");
+        let mut mac = HmacSha256::new_varkey(&self.hmac_key)
+            .expect("error while creating MAC instance from key");
         mac.input(input);
         let mut code = Vec::from(mac.result().code().as_ref());
         code.truncate(MAC_LENGTH);
@@ -79,7 +79,11 @@ impl EncryptionKeys {
     // verfies the MAC
     pub fn verify_mac(&self, input: &[u8], code_bytes: &[u8]) -> bool {
         let new_code = self.authenticate(input);
-        new_code == code_bytes
+        new_code.len() == code_bytes.len()
+            && (new_code
+                .iter()
+                .zip(code_bytes)
+                .fold(0, |acc, (x, y)| acc | (x ^ y)) == 0)
     }
 }
 
@@ -95,10 +99,11 @@ mod test {
     fn check_encryption() {
         let enc_keys = get_encrption_keys();
         let msg = b"input msg";
-        let cipher_text = enc_keys.encrypt(msg).expect("error while encrypting with OpenSSL");
+        let cipher_text = enc_keys
+            .encrypt(msg)
+            .expect("error while encrypting with OpenSSL");
         let decrypted_msg = enc_keys.decrypt(&cipher_text).unwrap();
         assert_eq!(msg, &decrypted_msg[..])
-
     }
 
     #[test]
--- tests/pkcs7.rs
+++ tests/pkcs7.rs
@@ -1,25 +1,19 @@
-extern crate rustc_serialize;
 extern crate openssl;
+extern crate rustc_serialize;
 
-use rustc_serialize::{
-    hex::FromHex,
-    base64::{
-        self,
-        ToBase64
-    }
-};
 use openssl::symm::{encrypt, Cipher};
-use std::process::{
-    Command,
-    Stdio
+use rustc_serialize::{
+    base64::{self, ToBase64},
+    hex::FromHex,
 };
+use std::process::{Command, Stdio};
 
 #[test]
 fn check_for_padding() {
     let _ = Command::new("pip3")
-                .args(&["install", "--user", "cryptography"])
-                .output();
-    
+        .args(&["install", "--user", "cryptography"])
+        .output();
+
     let py_script = "
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -45,26 +39,29 @@ print(encrypt('hello', key, iv).decode('utf-8'))
 ";
 
     let py_script = Command::new("echo")
-                .arg(py_script)
-                .stdout(Stdio::piped())
-                .spawn()
-                .expect("Could not echo the script")
-                .stdout
-                .expect("failed to get the stdout for py script");
+        .arg(py_script)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Could not echo the script")
+        .stdout
+        .expect("failed to get the stdout for py script");
 
     let py_script = Command::new("python3")
-                        .stdin(Stdio::from(py_script))
-                        .output()
-                        .expect("Could not execute the pyscript");
-    
-    let ciphertext_py = String::from_utf8(py_script.stdout).expect("could not get the stdout from py script");
+        .stdin(Stdio::from(py_script))
+        .output()
+        .expect("Could not execute the pyscript");
+
+    let ciphertext_py =
+        String::from_utf8(py_script.stdout).expect("could not get the stdout from py script");
     let ciphertext_py = ciphertext_py.trim();
-    
+
     let cipher = Cipher::aes_256_cbc();
-    let key = str::from_hex("5EBE2294ECD0E0F08EAB7690D2A6EE6926AE5CC854E36B6BDFCA366848DEA6BB").expect("Can't get key from hex");
+    let key = str::from_hex("5EBE2294ECD0E0F08EAB7690D2A6EE6926AE5CC854E36B6BDFCA366848DEA6BB")
+        .expect("Can't get key from hex");
     let iv = str::from_hex("E8C80B4B831FBB64B0D5C6C8499E541A").expect("Can't get iv from hex");
-    let ciphertext_rs = encrypt(cipher, &key, Some(&iv), b"hello").unwrap().to_base64(base64::STANDARD);
-    
+    let ciphertext_rs = encrypt(cipher, &key, Some(&iv), b"hello")
+        .unwrap()
+        .to_base64(base64::STANDARD);
+
     assert_eq!(ciphertext_py, ciphertext_rs);
 }
-
